9161192bf2defe7de51f0ebbb23d410beab8f443
Dmitry Vysotsky <bigbenperl@gmail.com>
2020-11-26 08:51:25 +0300

    Update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -11,3 +11,8 @@ config :results, ResultsWeb.Endpoint,
   code_reloader: false
 
 config :logger, level: :info
+
+config :results, Results.Worker,
+  python: "/home/deploy/anaconda3/envs/py37/bin/python",
+  detect_script: "/home/deploy/results/builds/python_scripts/detect.py",
+  model: {:system, "YOLO_MODEL"}
